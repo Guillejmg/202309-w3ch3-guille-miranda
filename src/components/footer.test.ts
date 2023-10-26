d393b47daa94807067e524eb60b0229447c9bbb8
Guille <g.mirandagaleote@gmail.com>
2023-10-26 02:07:26 +0200

    Fix indent style error in footer test

--- src/components/footer.test.ts
+++ src/components/footer.test.ts
@@ -6,7 +6,7 @@ import { Footer } from './footer';
 
 describe ('given footer component',()=>{
   describe ('when we instate it', ()=>{
-	    document.body.innerHTML='<div></div>';
+	     document.body.innerHTML='<div></div>';
     const footer =new Footer('div');
     test('then it should be instance of footer', () => {
       expect(footer.element).toBeInstanceOf(HTMLElement)
